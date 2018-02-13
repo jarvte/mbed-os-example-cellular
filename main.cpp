c9337962ce505abe4372d27e11697262c5bfd1ad
Teppo Järvelin <teppo.jarvelin@arm.com>
2018-02-13 11:09:48 +0200

    Updated license text, removed default sim and apn. Now gives pragma warning if sim pin is missing.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
 /*
- * Copyright (c) 2017 ARM Limited. All rights reserved.
+ * Copyright (c) 2017, Arm Limited and affiliates.
  * SPDX-License-Identifier: Apache-2.0
- * Licensed under the Apache License, Version 2.0 (the License); you may
- * not use this file except in compliance with the License.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an AS IS BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
@@ -22,14 +23,15 @@
 #define UDP 0
 #define TCP 1
 
-// SIM pin code goes here
 #ifndef MBED_CONF_APP_CELLULAR_SIM_PIN
-# define MBED_CONF_APP_CELLULAR_SIM_PIN    "1234"
+#warning "PIN Code is not defined"
+#define MBED_CONF_APP_CELLULAR_SIM_PIN  NULL
 #endif
 
 #ifndef MBED_CONF_APP_APN
-# define MBED_CONF_APP_APN         "internet"
+#define MBED_CONF_APP_APN   NULL
 #endif
+
 #ifndef MBED_CONF_APP_USERNAME
 # define MBED_CONF_APP_USERNAME    NULL
 #endif
